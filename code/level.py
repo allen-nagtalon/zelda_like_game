8cd462ec4e326bce5e3f127f27d6609db3aad912
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2022-02-01 16:22:37 -0800

    World map in settings is readable and displayable

---
+++ code/level.py
@@ -0,0 +1,32 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+
+class Level:
+  def __init__(self):
+
+    # Get the Display Surface
+    self.display_surface = pygame.display.get_surface()
+
+    # Sprite Group Setup
+    self.visible_sprites = pygame.sprite.Group()
+    self.obstacles_sprites = pygame.sprite.Group()
+
+    # Sprite Setup
+    self.create_map()
+
+  def create_map(self):
+    for r_ind, row in enumerate(WORLD_MAP):
+      for c_ind, tile in enumerate(row):
+        x = c_ind * TILESIZE
+        y = r_ind * TILESIZE
+
+        if tile == 'x':
+          Tile((x, y), [self.visible_sprites])
+
+        if tile == 'p':
+          Player((x, y), [self.visible_sprites])
+
+  def run(self):
+    self.visible_sprites.draw(self.display_surface)
--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
   def __init__(self):
@@ -9,6 +10,8 @@ class Game:
     pygame.display.set_caption('A Zelda Clone')
     self.clock = pygame.time.Clock()
 
+    self.level = Level()
+
   def run(self):
     while True:
       for event in pygame.event.get():
@@ -17,6 +20,7 @@ class Game:
           sys.exit()
 
       self.screen.fill('black')
+      self.level.run()
       pygame.display.update()
       self.clock.tick(FPS)
 
