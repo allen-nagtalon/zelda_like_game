2bd8f6116a832b63b4f43965ada7f9c1b69eae2d
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2022-02-01 19:44:57 -0800

    Added basic movement and inputs

--- code/player.py
+++ code/player.py
@@ -6,3 +6,31 @@ class Player(pygame.sprite.Sprite):
     super().__init__(groups)
     self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
     self.rect = self.image.get_rect(topleft = pos)
+
+    self.direction = pygame.math.Vector2()
+    self.speed = 5
+
+  def input(self):
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_UP]:
+      self.direction.y = -1
+    elif keys_pressed[pygame.K_DOWN]:
+      self.direction.y = 1
+    else:
+      self.direction.y = 0
+    
+    if keys_pressed[pygame.K_LEFT]:
+      self.direction.x = -1
+    elif keys_pressed[pygame.K_RIGHT]:
+      self.direction.x = 1
+    else:
+      self.direction.x = 0
+
+  def move(self, speed):
+    if self.direction.magnitude() != 0:
+      self.direction = self.direction.normalize
+    self.rect.center += self.direction * speed
+    
+  def update(self):
+    self.input()
+    self.move(self.speed)
