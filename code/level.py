898e89f97377fb010d0028cc755f0a44a3402c35
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2022-02-06 00:17:52 -0800

    Created setup for basic magic

--- code/level.py
+++ code/level.py
@@ -29,6 +29,9 @@ class Level:
   def create_attack(self):
     self.current_attack = Weapon(self.player, [self.visible_sprites])
 
+  def create_magic(self, style, strength, cost):
+    pass
+
   def create_map(self):
     layouts = {
       'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
@@ -55,7 +58,14 @@ class Level:
               surf = graphics['large_objects'][int(tile)]
               Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'large_object', surf)
 
-    self.player = Player((2300, 1400), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+    self.player = Player(
+      (2300, 1400), 
+      [self.visible_sprites], 
+      self.obstacle_sprites, 
+      self.create_attack, 
+      self.destroy_attack,
+      self.create_magic
+    )
 
   def destroy_attack(self):
     if self.current_attack:
